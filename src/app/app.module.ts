7b18998f9a3898545452e11c4fff9fe3ce7200de
Loic TRUCHOT <loic@truchot.be>
2017-04-26 16:33:19 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { SimpleTinyComponent } from './tinymce.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleTinyComponent
   ],
   imports: [
     BrowserModule,
